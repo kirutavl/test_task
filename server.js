071af7d8413335d4346e985572d3dcb770a49111
Slava <kirutavl@gmail.com>
2013-12-08 23:54:42 +0200

    presentation

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var util = require('util'),
-   
-   connect = require('connect'),
+    express = require('express'),
+    fs = require('fs'),
+    connect = require('connect'),
+    app = express(),
     port = process.env.PORT || 1337;
-var fs = require('fs');
+
 connect.createServer(connect.static(__dirname)).listen(port);
 util.puts('Listening on ' + port + '...');
 util.puts('Press Ctrl + C to stop.');
 fs.readdir('slides', function (err, stats) {
     if (err) throw err;
     console.log('stats: ' + JSON.stringify(stats));
+
+    app.get('/', function(req,res) {
+        res.send(JSON.stringify(stats));
+    });
 });
